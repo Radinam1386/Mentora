b1e018dbd0552a132dab70a0e6ff4c7524d47c0a
Kian Almasi <122436242+kian-al@users.noreply.github.com>
2026-06-26 14:01:03 +0330

    chore: setup database configuration for PostgreSQL support

--- backend/mentora/settings.py
+++ backend/mentora/settings.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 
 from dotenv import load_dotenv
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 PROJECT_ROOT = BASE_DIR.parent
@@ -75,10 +76,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mentora.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 LANGUAGE_CODE = 'fa-ir'
